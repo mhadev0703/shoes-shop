e9ffd57711197153bea59c5ad597175820dc29fe
하민철 <minchulha0703@gmail.com>
2024-01-24 02:17:08 -0500

    Implement createSlice/useDispatch for changing state in Redux

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Table } from 'react-bootstrap';
+import { useDispatch, useSelector } from "react-redux";
+import { changeName } from './../store.js';
 
 function Cart() {
+
+    // useSelector() brings Redux store return statement can be => state.stock
+    let userSelector = useSelector((state) => { return state.user })
+
+    let cartSelector = useSelector((state) => { return state.cart })
+    // send request to store.js
+    let dispatch = useDispatch()
+
     return(
         <div>
+            { userSelector }'s cart
             <Table>
                 <thead>
                     <tr>
@@ -14,12 +25,21 @@ function Cart() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>Hi</td>
-                    <td>Hi</td>
-                    <td>Hi</td>
-                    </tr>
+                    {
+                        cartSelector.map((a, i) => 
+                            <tr key={i}>
+                                <td>{1 + i}</td>
+                                <td>{ cartSelector[i].name }</td>
+                                <td>{ cartSelector[i].count}</td>
+                                <td>
+                                    <button onClick={() => {
+                                        // request store.js to do changeName()
+                                        dispatch(changeName())
+                                    }}>+</button>
+                                </td>
+                            </tr>
+                        )
+                    }
                 </tbody>
             </Table> 
         </div>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,30 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
 
+
+let user = createSlice({
+    name : 'user',
+    initialState : 'Jason',
+    reducers : {
+        changeName(state) {
+            return 'Nick'
+        }
+    }
+})
+
+export let { changeName } = user.actions
+
+let cart = createSlice({
+    name : 'cart',
+    initialState : 
+    [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+    ]
+})
 
 export default configureStore({
     reducer: {
-        
+        user : user.reducer,
+        cart : cart.reducer
     }
 })
