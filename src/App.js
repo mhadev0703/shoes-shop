7f8a32626a6c50507790dcc36d7767fe7ba5cdf2
하민철 <minchulha0703@gmail.com>
2024-02-03 23:55:39 -0500

    Add lazy import & suspense for shorter load times

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { createContext, useEffect, useState } from "react";
+import { lazy, Suspense, createContext, useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 import data from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet, useParams } from 'react-router-dom';
-import Detail from './pages/Detail.js';
-import Cart from './pages/Cart.js';
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
+const Detail = lazy(() => import('./pages/Detail.js'));
+const Cart = lazy(() => import('./pages/Cart.js'));
 
 export let Context1 = createContext()
 
@@ -43,94 +43,98 @@ function App() {
     <div className="App">      
 
       <Navbar bg="lightgrey" variant="lightgrey" className='navbar'>
-        <Container>
-          <Navbar.Brand>ReactStride</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
-            <Nav.Link onClick={() => { navigate('/detail/0') }}>Detail</Nav.Link>
-            <Nav.Link onClick={() => { navigate('/cart') }}>Cart</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">Hello, 
+        <Container fluid>
+          <div className="d-flex justify-content-between" style={{ margin: '0 20px' }}>
+            <Navbar.Brand>ReactStride</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/detail/0') }}>Detail</Nav.Link>
+              <Nav.Link onClick={() => { navigate('/cart') }}>Cart</Nav.Link>
+            </Nav>
+          </div>
+          <div className="greeting" style={{ margin: '0 20px' }}>
+            Hello,&nbsp;
             { result.isLoading && 'Loading...' }
-            { result.error && '에러남' }
+            { result.error && 'Error' }
             { result.data && result.data.name }
-          </Nav>
+          </div>
         </Container>
       </Navbar>      
 
-      <Routes>
-        <Route path="/" element={
-          <>
-          <div className="main-bg"></div>
-          <Container>
-            <Row>
-              { shoes.map(function (shoe, i) {
-                return <Card shoes={shoe} i={i+1} />;
-              })}
-            </Row>
-          </Container>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={
+            <>
+            <div className="main-bg"></div>
+            <Container>
+              <Row>
+                { shoes.map(function (shoe, i) {
+                  return <Card shoes={shoe} i={i+1} />;
+                })}
+              </Row>
+            </Container>
 
-          <button onClick={() => {
+            <button onClick={() => {
 
-            setClickCount(prev => prev + 1);
-            let url = '';
-            
-            if (clickCount === 1) {
-              url = 'https://codingapple1.github.io/shop/data2.json';
-            } else if (clickCount === 2) {
-              url = 'https://codingapple1.github.io/shop/data3.json'
-            } else if (clickCount >= 3) {
-              alert('There are no more items to show.');
-              return;
-            }
+              setClickCount(prev => prev + 1);
+              let url = '';
+              
+              if (clickCount === 1) {
+                url = 'https://codingapple1.github.io/shop/data2.json';
+              } else if (clickCount === 2) {
+                url = 'https://codingapple1.github.io/shop/data3.json'
+              } else if (clickCount >= 3) {
+                alert('There are no more items to show.');
+                return;
+              }
 
-            setIsLoading(true);
+              setIsLoading(true);
 
-            axios.get(url)
-            .then((result) => {
-              const convertedData = result.data.map((item) => ({
-                ...item,
-                price: convertPrice(item.price), 
-              }));
+              axios.get(url)
+              .then((result) => {
+                const convertedData = result.data.map((item) => ({
+                  ...item,
+                  price: convertPrice(item.price), 
+                }));
 
-              let copiedShoes = [...shoes, ...convertedData];
-              setShoes(copiedShoes);
-            })
-            .catch(() => {
-              console.log('Connection failed')
-            })
-            .finally(() => {
-              // stop the loading process after data fetching is complete
-              setIsLoading(false);
-            })
-          }}>See more</button>
-          {isLoading && <div>Loading...</div>}
-          </>
-        } />
+                let copiedShoes = [...shoes, ...convertedData];
+                setShoes(copiedShoes);
+              })
+              .catch(() => {
+                console.log('Connection failed')
+              })
+              .finally(() => {
+                // stop the loading process after data fetching is complete
+                setIsLoading(false);
+              })
+            }}>See more</button>
+            {isLoading && <div>Loading...</div>}
+            </>
+          } />
 
-        <Route path="/detail/:id" element={
-          <Context1.Provider value={{ stock }}>
-            <Detail shoes={shoes} />
-          </Context1.Provider>
-        } />
+          <Route path="/detail/:id" element={
+            <Context1.Provider value={{ stock }}>
+              <Detail shoes={shoes} />
+            </Context1.Provider>
+          } />
 
-        <Route path="/cart" element={ <Cart/> }>
+          <Route path="/cart" element={ <Cart/> }>
 
-        </Route>
+          </Route>
 
-        <Route path="/about" element={<About/>}>
-          <Route path="member" element={<div>Members</div>} />
-          <Route path="location" element={<div>Location</div>} />
-        </Route>
+          <Route path="/about" element={<About/>}>
+            <Route path="member" element={<div>Members</div>} />
+            <Route path="location" element={<div>Location</div>} />
+          </Route>
 
-        <Route path="/event" element={<Event/>}>
-          <Route path="bonus" element={<div>Bonus item for your first order 🎁</div>} />
-          <Route path="coupon" element={<div>Get your birthday coupon 🎉</div>} />
-        </Route>
-        
-        <Route path="*" element={<div>The Page doesn't exist.</div>} />
-      </Routes>
-
+          <Route path="/event" element={<Event/>}>
+            <Route path="bonus" element={<div>Bonus item for your first order 🎁</div>} />
+            <Route path="coupon" element={<div>Get your birthday coupon 🎉</div>} />
+          </Route>
+          
+          <Route path="*" element={<div>The Page doesn't exist.</div>} />
+        </Routes>
+      </Suspense>
     </div>
   );  
 }
